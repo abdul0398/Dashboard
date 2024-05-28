a073d01e6708c2876b621e4a6be6a7181d9227db
abdul0398 <abdulrkhan570@gmail.com>
2024-05-28 16:42:18 +0530

    fix:changed form rdirect url

--- src/app/dashboard/condo-rental-rates/page.tsx
+++ src/app/dashboard/condo-rental-rates/page.tsx
@@ -1,5 +1,23 @@
+"use client";
+import { useSearchParams } from "next/navigation";
+
 export default function NewListing() {
-    return (
-        <iframe src="https://condo-rent-dashboard.vercel.app/" className="w-full h-full"></iframe>
-    )
+  const params = useSearchParams();
+  const project = params.get("project");
+
+  return (
+    <>
+      {project ? (
+        <iframe
+          src={`https://condo-rent-dashboard.vercel.app/?project=${project}`}
+          className="w-full h-full"
+        />
+      ) : (
+        <iframe
+          src="https://condo-rent-dashboard.vercel.app/"
+          className="w-full h-full"
+        />
+      )}
+    </>
+  );
 }
--- src/app/dashboard/hdb-market-rental-rates/page.tsx
+++ src/app/dashboard/hdb-market-rental-rates/page.tsx
@@ -1,5 +1,25 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+
 export default function NewListing() {
-    return (
-        <iframe src="https://hdb-rental-portal.vercel.app/" className="w-full h-full"></iframe>
-    )
+  const params = useSearchParams();
+  const street = params.get("street");
+  const block = params.get("block");
+  const flatType = params.get("flatType");
+  return (
+    <>
+      {street || block || flatType ? (
+        <iframe
+          src={`https://hdb-rental-portal.vercel.app/?street=${street}&block=${block}&flatType=${flatType}`}
+          className="w-full h-full"
+        />
+      ) : (
+        <iframe
+          src="https://hdb-rental-portal.vercel.app/"
+          className="w-full h-full"
+        />
+      )}
+    </>
+  );
 }
