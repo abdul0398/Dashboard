eb5769987dfd3e46435d58133b38d8a4f55ac006
abdul0398 <abdulrkhan570@gmail.com>
2024-09-19 13:03:20 +0530

    Added: params

--- src/app/dashboard/hbd-market-rental-rates/page.tsx
+++ src/app/dashboard/hbd-market-rental-rates/page.tsx
@@ -5,18 +5,11 @@ import { Suspense, useEffect, useState } from "react";
 
 function NewListing() {
   const searchParams = useSearchParams();
-  const [url, setUrl] = useState<string | null>(null);
-
-  useEffect(() => {
     const street = searchParams.get("street");
     const block = searchParams.get("block");
 
-    if (street && block) {
-      setUrl(`https://hdb-rental-portal.vercel.app/?street=${street}&block=${block}`);
-    }
-  }, [searchParams]);
+    let url = `https://hdb-rental-portal.vercel.app/?street=${street}&block=${block}`;
 
-  if (!url) return null;
 
   return (
     <iframe src={url} className="w-full h-full"></iframe>
